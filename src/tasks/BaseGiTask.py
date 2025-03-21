45cfaf6608282a9a643a905e2f85c7856977ba64
firedcto@gmail.com <firedcto@gmail.com>
2025-03-21 18:27:09 +0800

    修复有时候截图方式使用错误和hdr判断错误

--- src/tasks/BaseGiTask.py
+++ src/tasks/BaseGiTask.py
@@ -400,8 +400,8 @@ class BaseGiTask(BaseTask):
         self.ensure_main()
         self.open_book()
         self.click_relative(0.16, 0.42, after_sleep=1)
-        self.click_relative(0.47, 0.2, after_sleep=0.5)
-        self.click_relative(0.46, 0.31, after_sleep=0.5)
+        self.click_relative(0.47, 0.2, after_sleep=1)
+        self.click_relative(0.46, 0.31, after_sleep=1)
 
     def scroll_into_relic(self, n, use_resin):
         self.go_to_relic()
