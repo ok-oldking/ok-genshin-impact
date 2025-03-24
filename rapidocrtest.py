6d8c256c67ae3fa2745116dda67996d80a2ca033
firedcto@gmail.com <firedcto@gmail.com>
2025-03-24 23:07:34 +0800

    修复无法记录游戏启动路径

--- rapidocrtest.py
+++ rapidocrtest.py
@@ -10,9 +10,12 @@ print(f'xxxxx cur_window_version_str="{cur_window_version_str}"')
 engine = RapidOCR(params={"Global.lang_det": "ch_mobile", "Global.lang_rec": "ch_mobile", "Global.use_cls": False,
                           "EngineConfig.onnxruntime.use_dml": True})
 
-img = cv2.imread('tests/images/end_battle.png')
+img = cv2.imread('tests/images/pick.png')
 # img = cv2.resize(img, (1280, 720))
-for i in range(20):
-    result = engine(img, use_cls=False)
-    print(result.elapse)
-# print(result)
+
+result = engine(img, use_cls=False)
+print(result)
+
+# for i in range(20):
+#     result = engine(img, use_cls=False)
+#     print(result.elapse)
