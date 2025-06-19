7b4077669f64c3a0516b9e0106886f8bbb6574a8
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-06-19 10:19:41 +0800

    不再强制要求关闭HDR

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ config = {
         'can_bit_blt': True,  # default false, opengl games does not support bit_blt
         'bit_blt_render_full': True,
         'check_hdr': True,
-        'force_no_hdr': True,
+        # 'force_no_hdr': True,
         # 'check_night_light': True,
         'force_no_night_light': False,
         'require_bg': True
